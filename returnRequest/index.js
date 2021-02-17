45f082c7e9b52b57b1803677053a1e56f6ccaa2e
alexfritsch10 <alexfritsch10@gmail.com>
2021-02-17 12:28:11 +0100

    Test Outputs

--- returnRequest/index.js
+++ returnRequest/index.js
@@ -1,4 +1,8 @@
-module.exports = (req, res) => {  
-    res.send(req.toString() + "\n");
+module.exports = (req, res) => {
+    console.log(req)
+    console.log(req.data)
+    console.log(req.Data)
+    console.log(req.Body)
+    console.log(req.body)
+    res.send(req);
 }
-  
