b87f6ae1fd4e396c637f9c08dea178b22d8d6c25
Alexander Fritsch <45079854+alexfritsch10@users.noreply.github.com>
2021-02-18 20:25:20 +0100

    Update index.js

--- returnRequest/index.js
+++ returnRequest/index.js
@@ -1,5 +1,6 @@
 module.exports = (req, res) => {
     console.log("------------___________________-----------------")
+    console.log(req.ParseForm())
     console.log(req)
     console.log("----------------------------------------------------")
     res.send(req);
