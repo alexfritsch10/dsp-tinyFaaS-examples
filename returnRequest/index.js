3631f1d36cc37aca0ccc8f221de1ff9c981f3f0c
Alexander Fritsch <45079854+alexfritsch10@users.noreply.github.com>
2021-02-19 00:52:27 +0100

    Update index.js

--- returnRequest/index.js
+++ returnRequest/index.js
@@ -1,6 +1,9 @@
 module.exports = (req, res) => {
-    console.log("------------___________________-----------------")
-    console.log(req)
-    console.log("----------------------------------------------------")
+    console.log("------------___________________-----------------");
+    console.log(req);
+    let keyVal = req.url.split("/");
+    console.log("Received Key: " + keyVal[0]);
+    console.log("Received Value: " + keyVal[1]);
+    console.log("----------------------------------------------------");
     res.send(req.body);
 }
