d60b360cb1c4d046a1330e0735064e9a89fbdb4c
Alexander Fritsch <alexfritsch10@gmail.com>
2021-02-22 10:56:19 +0100

    Delete unused packages and change func. response

--- printData/index.js
+++ printData/index.js
@@ -1,5 +1,3 @@
-const axios = require('axios');
-
 module.exports = (req, res) => { 
     console.log("Received Key: " + keyVal[1]);
     console.log("Received Value: " + keyVal[2]);
@@ -12,20 +10,6 @@ module.exports = (req, res) => {
         "key": keyVal[1],
         "value": keyVal[2]
     }
-    // the ip address depends on the local machine 
-    axios.post("http://172.21.0.1:8081", data, {
-        headers: {
-          'Content-Type': 'application/json',
-        }
-      })
-        .then((response) => {
-            console.log(response);
-            callback(response.data.Status, response.data.Message);
-        })
-        .catch((error) => {
-            console.log(error);
-            callback('400', 'Check for API Connection!');
-        });
 
-    res.send(req.body);
+    res.send(data);
 }
